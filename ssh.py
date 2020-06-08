03bddbb8e8cf7ea895c6c529ba1b711ebec1a8e7
IndominusByte <nyomanpradipta120@gmail.com>
2020-06-08 15:55:39 +0800

    update ssh

--- ssh.py
+++ ssh.py
@@ -1,7 +1,7 @@
 from subprocess import PIPE, Popen
 
 cmd = 'uname -a'
-stream = Popen(['ssh', 'okky@192.168.18.11', cmd],stdin=PIPE, stdout=PIPE)
+stream = Popen(['ssh', 'nyoman@192.168.18.48', cmd],stdin=PIPE, stdout=PIPE)
 
 rsp = stream.stdout.read().decode('utf-8')
 print(rsp)
